2922975e9ca51c35d30168230faf5b10af6b1d44
Kevin Chen <kevinmichaelchen@gmail.com>
2024-10-25 18:31:41 -0400

    feat: Is this thing on?

--- packages/scripts/src/example.ts
+++ packages/scripts/src/example.ts
@@ -1,4 +1,4 @@
 import { Resource } from "sst";
-import { Example } from "@monorepo-template/core/example";
+import { Example } from "@acme/core/example";
 
 console.log(`${Example.hello()} Linked to ${Resource.MyBucket.name}.`);
--- sst.config.ts
+++ sst.config.ts
@@ -3,7 +3,7 @@
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
+      name: "acme",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
     };
